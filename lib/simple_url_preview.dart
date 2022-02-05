0e871c0709f520fcb721841fa2a30cf33b4cae7c
Nightbl927 <37490140+Nightbl927@users.noreply.github.com>
2022-02-05 08:31:26 -0500

    Update simple_url_preview.dart

--- lib/simple_url_preview.dart
+++ lib/simple_url_preview.dart
@@ -2,7 +2,7 @@ library simple_url_preview;
 
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:html/dom.dart';
+import 'package:html/dom.dart' as dom;
 import 'package:html/parser.dart';
 import 'package:http/http.dart';
 import 'package:simple_url_preview/widgets/preview_description.dart';
@@ -11,6 +11,7 @@ import 'package:simple_url_preview/widgets/preview_site_name.dart';
 import 'package:simple_url_preview/widgets/preview_title.dart';
 import 'package:string_validator/string_validator.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 /// Provides URL preview
 class SimpleUrlPreview extends StatefulWidget {
@@ -63,12 +64,12 @@ class SimpleUrlPreview extends StatefulWidget {
     this.imageLoaderColor,
     this.previewContainerPadding,
     this.onTap,
-  })  : assert(previewHeight >= 130.0,
-            'The preview height should be greater than or equal to 130'),
-        assert(titleLines <= 2 && titleLines > 0,
-            'The title lines should be less than or equal to 2 and not equal to 0'),
-        assert(descriptionLines <= 3 && descriptionLines > 0,
-            'The description lines should be less than or equal to 3 and not equal to 0');
+  }) : assert(previewHeight >= 130.0,
+        'The preview height should be greater than or equal to 130'),
+      assert(titleLines <= 2 && titleLines > 0,
+        'The title lines should be less than or equal to 2 and not equal to 0'),
+      assert(descriptionLines <= 3 && descriptionLines > 0,
+        'The description lines should be less than or equal to 3 and not equal to 0');
 
   @override
   _SimpleUrlPreviewState createState() => _SimpleUrlPreviewState();
@@ -89,15 +90,11 @@ class _SimpleUrlPreviewState extends State<SimpleUrlPreview> {
   EdgeInsetsGeometry? _previewContainerPadding;
   VoidCallback? _onTap;
 
+  bool _loading = false;
+
   @override
   void initState() {
     super.initState();
-    _getUrlData();
-  }
-
-  @override
-  void didUpdateWidget(SimpleUrlPreview oldWidget) {
-    super.didUpdateWidget(oldWidget);
     _getUrlData();
   }
 
@@ -113,6 +110,10 @@ class _SimpleUrlPreviewState extends State<SimpleUrlPreview> {
   }
 
   void _getUrlData() async {
+    setState(() {
+      _loading = true;
+    });
+
     if (!isURL(widget.url)) {
       setState(() {
         _urlPreviewData = null;
@@ -147,9 +148,13 @@ class _SimpleUrlPreviewState extends State<SimpleUrlPreview> {
         _isVisible = true;
       });
     }
+
+    setState(() {
+      _loading = false;
+    });
   }
 
-  void _extractOGData(Document document, Map data, String parameter) {
+  void _extractOGData(dom.Document document, Map data, String parameter) {
     var titleMetaTag = document
       .getElementsByTagName("meta")
       .firstWhereOrNull((meta) => meta.attributes['property'] == parameter);
@@ -196,7 +201,7 @@ class _SimpleUrlPreviewState extends State<SimpleUrlPreview> {
     }
   }
 
-  void _extractStandardData(Document document, Map data, String parameter) {
+  void _extractStandardData(dom.Document document, Map data, String parameter) {
     var titleMetaTag = document
         .getElementsByTagName("meta")
         .firstWhereOrNull((meta) => meta.attributes['property'] == parameter);
@@ -217,71 +222,72 @@ class _SimpleUrlPreviewState extends State<SimpleUrlPreview> {
   Widget build(BuildContext context) {
     _isClosable = widget.isClosable ?? false;
     _bgColor = widget.bgColor ?? Theme.of(context).primaryColor;
-    _imageLoaderColor =
-        widget.imageLoaderColor ?? Theme.of(context).accentColor;
+    _imageLoaderColor = widget.imageLoaderColor ?? Theme.of(context).accentColor;
     _initialize();
 
-    if (_urlPreviewData == null || !_isVisible) {
-      return SizedBox();
-    }
-
-    return ConstrainedBox(
-      constraints: BoxConstraints(
-        minHeight: _previewHeight!,
-      ),
-      child: Container(
+    return Container(
+      child: _loading == false ? (_urlPreviewData == null || !_isVisible) ? SizedBox.shrink() : Container(
         padding: _previewContainerPadding,
-        child: Stack(
-          children: [
-            GestureDetector(
-              onTap: _onTap,
-              child: _buildPreviewCard(context),
+        child: GestureDetector(
+          onTap: _onTap,
+          onLongPress: () {},
+          child: _buildPreviewCard(context),
+        ), //_buildClosablePreview(),
+      ) : Card(
+        margin: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+        elevation: 0,
+        color: _bgColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+            child: SpinKitThreeBounce(
+              color: Theme.of(context).indicatorColor,
+              size: 50.0,
             ),
-            _buildClosablePreview(),
-          ],
+          )
         ),
       ),
     );
   }
 
   Widget _buildClosablePreview() {
-    return _isClosable
-        ? Align(
-            alignment: Alignment.topRight,
-            child: IconButton(
-              icon: Icon(
-                Icons.clear,
-              ),
-              onPressed: () {
-                setState(() {
-                  _isVisible = false;
-                });
-              },
-            ),
-          )
-        : SizedBox();
+    return _isClosable ? Align(
+      alignment: Alignment.topRight,
+      child: IconButton(
+        icon: Icon(
+          Icons.clear,
+        ),
+        onPressed: () {
+          setState(() {
+            _isVisible = false;
+          });
+        },
+      ),
+    ) : SizedBox();
   }
 
   Card _buildPreviewCard(BuildContext context) {
     return Card(
-      margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
+      margin: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
       elevation: 0,
       color: _bgColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
-        side: BorderSide(color: Theme.of(context).indicatorColor, width: 0.5)
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           PreviewImage(
             _urlPreviewData!['og:image'],
             _imageLoaderColor,
           ),
-          Flexible(
-            fit: FlexFit.loose,
+          Expanded(
             child: Padding(
-              padding: EdgeInsets.all(16.0),
+              padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
